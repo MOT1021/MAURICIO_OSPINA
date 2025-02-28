35504c38ded54ca563944c6a5c01e875e0b32dbf
MOT1021 <juan.ospina68@est.iudigital.edu.co>
2025-02-28 00:40:23 -0500

    actividad_1_POO

---
+++ src/activida1.py
@@ -0,0 +1,9 @@
+from Clase1 import Personas
+
+persona = Personas()
+persona.Edad = 70
+persona.Altura = 175.6
+persona.Peso = 90
+print(persona.Nombre,persona.Peso,persona.Altura)
+print(persona.Nombre,persona.Peso,persona.Altura)
+print(persona.Nombre,persona.Peso,"Hola Mundo")
