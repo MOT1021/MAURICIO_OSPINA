060c2cec5fdc5da3b17a0d0e293f1619a600f64b
MOT1021 <juan.ospina68@est.iudigital.edu.co>
2025-02-28 00:32:52 -0500

    actividad_1_POO

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="POO",
+    version="0.0.1",
+    author="JUAN MAURICIO OSPINA",
+    author_email="Juan.ospina68@est.iudigital.edu.co",
+    description="buenas practicas de la PAD",
+    py_modules=["actividad_1"],
+    install_requires=
+    [
+        "pandas",
+        "matplotlib"
+    ]
+)
