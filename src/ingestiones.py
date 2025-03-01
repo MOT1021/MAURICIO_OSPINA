98bee122687343966a0df9d0d186754fa10c97b1
MOT1021 <juan.ospina68@est.iudigital.edu.co>
2025-02-28 20:18:02 -0500

    actvidad 2

--- src/ingestiones.py
+++ src/ingestiones.py
@@ -1,4 +1,46 @@
 import json
+import requests
+
+# Inicializando el diccionario para contabilizar las tareas completadas por usuario
+tareas_completadas_por_usuario = {}
+
+
+
+url = "https://jsonplaceholder.typicode.com/todos"
+respuesta_api = requests.get(url)
+
+# Convertir la respuesta en formato JSON a una estructura de datos de Python
+lista_tareas = json.loads(respuesta_api.text)
+
+print(lista_tareas) 
+
+# Recorrer la lista de tareas
+for tarea in lista_tareas:
+    if tarea["completed"]:
+        usuario_id = tarea["userId"]
+        try:
+            # Intentar incrementar el contador para el usuario existente
+            tareas_completadas_por_usuario[usuario_id] += 1
+        except KeyError:
+            # Si el usuario no está en el diccionario, agregarlo con un contador de 1
+            tareas_completadas_por_usuario[usuario_id] = 1
+
+usuarios_ordenados = sorted(tareas_completadas_por_usuario.items(), key=lambda x: x[1], reverse=True)
+# Determinar el máximo número de tareas completadas
+max_completadas = usuarios_ordenados[0][1]
+usuarios_max = []
+
+for usuario, num_completadas in usuarios_ordenados:
+    if num_completadas < max_completadas:
+        break
+    usuarios_max.append(str(usuario))
+
+# Crear una cadena con los usuarios que más tareas completaron
+usuarios_max_str = " y ".join(usuarios_max)
+
+print(f"Usuario(s) {usuarios_max_str} completaron {max_completadas} tareas")
+
+
 
 class Ingestiones():    
     def __init__(self):
@@ -11,7 +53,8 @@ class Ingestiones():
         with open(ruta_json,"r",encoding="utf-8") as f:
             datos = json.load(f)
         return datos    
-    
+
+
     def leer_txt(self):
         # r read w write
 
@@ -20,6 +63,10 @@ class Ingestiones():
         with open(ruta_json,"r",encoding="utf-8") as f:
             datos = f.read()
         return datos 
+    
+  
+
+
 
     def leer_varios_txt(self,nombre=""):
         # r read w write
@@ -42,7 +89,7 @@ class Ingestiones():
     def  leer_bd(self,nombre_bd="",servidor="",puerto=0000):
         pass
     
-    def  leer_api(self,url=""):
+    def  leer_api(self,url="https://jsonplaceholder.typicode.com/todos"):
         pass
     
     def escribir_txt(self,nombre,datos):
@@ -66,6 +113,13 @@ print("************************************************************")
 nombre_archivo = "info copy.txt"
 datos_txt_dos = inges.leer_varios_txt(nombre_archivo)
 print(datos_txt_dos)
+print("************************************************************")
+print("************************************************************")
+print("Primeros dos elementos de la lista de tareas:", lista_tareas[:2])
+print("************************************************************")
+print("************************************************************")
+print(f"Usuario(s) {usuarios_max_str} completaron {max_completadas} tareas")
+
 
 inges.escribir_txt(nombre="archivo_json",datos=datos_json)
 inges.escribir_txt(nombre="archivo_txt",datos=datos_txt)
